dd9287aa83c4cc4f62d2becfeb0947b3a7f92c1b
Alejandro Gallarati <radamant24@gmail.com>
2017-12-29 16:01:04 -0300

    V1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CSVtoJSON NodeJS-Module1
+# CSVtoJSON NodeJS-Module1 by Radamant24
+# Execute node script.js and a parsedJSON file will be created.
---
+++ script.js
@@ -0,0 +1,21 @@
+const path = require("path")
+const fs = require("fs")
+const csv = require("csvtojson")
+
+const csvFilePath='customer-data.csv'
+var ObjectJSON = []
+
+csv()
+.fromFile(csvFilePath)
+.on('json',(jsonObj)=>{
+    ObjectJSON.push(jsonObj)
+
+})
+.on('done',(error)=>{
+	if (error) { return error}
+	fs.writeFile('jsonparsed.json', JSON.stringify(ObjectJSON), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+    });
+    console.log('end')
+})
